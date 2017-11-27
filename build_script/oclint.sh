9a39034f2387989236e8eb17e79abceeed30ca8e
zhoujihang <zhoujihang@xiaozhu.com>
2017-11-27 20:38:23 +0800

    新增 oclint.sh 加入代码扫描测试脚本

---
+++ build_script/oclint.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+
+echo 'begin oclint.sh ...'
+
+if which oclint 2>/dev/null; then
+    echo 'oclint exist'
+else
+    echo 'error:please install oclint!!'
+    exit -1
+fi
+if which xcpretty 2>/dev/null; then
+    echo 'xcpretty exist'
+else
+    echo 'error:please install xcpretty!!'
+    exit -1
+fi
+
+# 程序入口
+build_number=$1
+if [ -z $build_number ]; then
+build_number=0
+fi
+
+# 脚本执行所在目录
+script_file_dir="`dirname $0`"
+script_file_dir_prefix=${root_path:0:1}
+if [[ "$script_file_dir_prefix" != "/" ]]; then
+script_file_dir="`pwd`/${script_file_dir}"
+fi
+
+# 项目根目录
+root_path="`dirname $0`/.."
+root_path_prefix=${root_path:0:1}
+if [[ "$root_path_prefix" != "/" ]]; then
+root_path="`pwd`/${root_path}"
+fi
+src_path="${root_path}/CalendarDemo"
+output_path="${src_path}/b_output"
+
+xcodebuild="/usr/bin/xcodebuild" 
+
+# 重建b_output目录
+if [[ ! -z "$output_path" ]]  && [[ ! -z "$root_path" ]]; then
+rm -rf ${output_path}
+fi
+mkdir -pv ${output_path}
+
+# 进入项目根目录
+cd "${src_path}"
+
+# 使用xcpretty生成compile_commands.json文件
+"${xcodebuild}" -workspace CalendarDemo.xcworkspace -scheme CalendarDemo -configuration Release clean build |tee "${output_path}"/xcodebuild.log |xcpretty --report json-compilation-database --output "${output_path}"/compile_commands.json
+
+# 将compile_commands.json转化为oclint.xml
+cd "${output_path}"
+oclint-json-compilation-database -e Pods -v -- -max-priority-2=99999999 -max-priority-3=99999999 -report-type pmd -o oclint.xml
+
+echo 'finish oclint.sh ...'
